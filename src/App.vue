745adf5ad1b2a519b02531995b080fd80984db45
hamid.yuksel <h.yuksel04@gmail.com>
2019-02-05 20:22:39 -0500

    bug fix: links dont display

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,23 @@
 <template>
   <div id="app">
-
+    <div class="container">
+      <div>
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link to="/fiction">Fiction</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/poetry">Poetry</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/rambles">Rambles</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/contact">Contact</router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
     <router-view/>
   </div>
 </template>
