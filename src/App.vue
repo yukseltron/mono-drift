e29b75c9f89162f5f3e3d697836b02a29a5ad04e
hamid.yuksel <h.yuksel04@gmail.com>
2019-02-13 10:15:32 -0500

    updated title, orientation

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,30 @@
   <div id="app">
     <div class="container">
       <div>
-        <router-link to="/"><h1>Splash of Sublime</h1></router-link>
-        <ul class="navbar-nav">
-          <li class="nav-item">
+        <router-link to="/">
+          <div class="title"><pre>
+
+   ███╗   ███╗ ██████╗ ███╗   ██╗ ██████╗       ██████╗ ██████╗ ██╗███████╗████████╗
+    ████╗ ████║██╔═══██╗████╗  ██║██╔═══██╗      ██╔══██╗██╔══██╗██║██╔════╝╚══██╔══╝
+  ██╔████╔██║██║   ██║██╔██╗ ██║██║   ██║█████╗██║  ██║██████╔╝██║█████╗     ██║
+   ██║╚██╔╝██║██║   ██║██║╚██╗██║██║   ██║╚════╝██║  ██║██╔══██╗██║██╔══╝     ██║
+    ██║ ╚═╝ ██║╚██████╔╝██║ ╚████║╚██████╔╝      ██████╔╝██║  ██║██║██║        ██║
+      ╚═╝     ╚═╝ ╚═════╝ ╚═╝  ╚═══╝ ╚═════╝       ╚═════╝ ╚═╝  ╚═╝╚═╝╚═╝        ╚═╝
+
+
+          </pre></div></router-link>
+        <p>Sublime Sci-Fi Flash Fiction</p>
+        <ul class="stories">
+          <li class="story">
             <router-link to="/cactuscereal">Cactus Cereal</router-link>
           </li>
-          <li class="nav-item">
+          <li class="story">
             <router-link to="/chimera">Chimera</router-link>
           </li>
-          <li class="nav-item">
+          <li class="story">
             <router-link to="/dreamsofamonster">Dreams of a Monster</router-link>
           </li>
-          <li class="nav-item">
+          <li class="story">
             <router-link to="/thelastkingbalthazar">The Last King Balthazar</router-link>
           </li>
         </ul>
@@ -23,3 +35,28 @@
   </div>
 </template>
 <script>export default {name: 'App'}</script>
+<style>
+  #app {
+    font-family: 'VT323', monospace;
+    font-style: normal;
+  }
+
+  .container {
+    text-align: center;
+  }
+
+  .title {
+    color: black;
+    display: inline-block;
+    font-size: 2vmin;
+  }
+
+  .title:hover {
+    color: white;
+    background-color: black;
+  }
+
+  .story{
+    text-align: left;
+  }
+</style>
--- src/components/home.vue
+++ src/components/home.vue
@@ -1,5 +1,4 @@
 <template>
-  <div class="home">
-    <h1>Slash Fiction By Hamid</h1>
-  </div>
 </template>
+<style>
+</style>
