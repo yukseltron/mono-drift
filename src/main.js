13ae4915804713a2b0fe51eff3af87091312b82e
hamid.yuksel <h.yuksel04@gmail.com>
2019-02-21 15:34:46 -0500

    fixed scrolling bug

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+var VueScrollTo = require('vue-scrollto')
+Vue.use(VueScrollTo)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
@@ -12,21 +14,4 @@ new Vue({
   router,
   components: { App },
   template: '<App/>'
-})
-
-var VueScrollTo = require('vue-scrollto')
-
-// You can also pass in the default options
-Vue.use(VueScrollTo, {
-  container: 'body',
-  duration: 500,
-  easing: 'ease',
-  offset: 0,
-  force: true,
-  cancelable: true,
-  onStart: false,
-  onDone: false,
-  onCancel: false,
-  x: false,
-  y: true
 })
